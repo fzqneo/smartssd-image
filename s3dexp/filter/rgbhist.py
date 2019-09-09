83207c0fabc35588dc5f88be7545aa42c6117939
a4anna <shilpa.a.george@gmail.com>
2019-09-09 12:17:48 -0400

    Adding RGB 2D Color Filter

--- s3dexp/filter/rgbhist.py
+++ s3dexp/filter/rgbhist.py
@@ -1,8 +1,7 @@
-
+import itertools
 import cv2
 import numpy
 from s3dexp.search import Filter
-
 
 class RGBHist1dFilter(Filter):
     def __init__(self):
@@ -10,19 +9,28 @@ class RGBHist1dFilter(Filter):
 
     def __call__(self, item):
         hists = []
-        for i in range(0):  # channels
-            cv2.calcHist([item.array, ], [i, ], None, [256, ], [0, 256])
-            hists.append[i]
+        num_channels = len(item.array.shape)
+        for i in range(num_channels):  # channels
+            hist = cv2.calcHist([item.array, ], [i, ], None, [256, ], [0, 256])
+            hists.append(hist)
         item['rgb_hist_1d'] = hists
         return True
 
-
-# Reference: https://www.pyimagesearch.com/2014/01/22/clever-girl-a-guide-to-utilizing-color-histograms-for-computer-vision-and-image-search-engines/
 
 class RGBHist2dFilter(Filter):
     def __init__(self):
         super(RGBHist2dFilter, self).__init__()
 
     def __call__(self, item):
-        raise NotImplementedError
+        num_channels = len(item.array.shape)
+        if num_channels == 3:
+            hists = []
+            clr_iter = [i for i in itertools.combinations(range(num_channels),2)]
+            for (a, b) in clr_iter:
+                hist = cv2.calcHist([item.array,], [a, b], None, [32, 32], [0, 256, 0, 256])
+                hists.append(hist)
+        else:
+            hists = cv2.calcHist([item.array,], [0], None, [256, ], [0, 256])
+        item['rgb_hist_2d'] = hists
+        return True
 
