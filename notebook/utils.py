5805ca0957a95ba69114569ad2c23839a8a8ae07
Ziqiang feng <zf@cs.cmu.edu>
2019-09-23 17:29:40 -0400

    Checkpoint: first complete draft of paper (modulo conclusion). Add macro benchmark notebook.

--- notebook/utils.py
+++ notebook/utils.py
@@ -4,6 +4,9 @@ def get_by_expnames(df, expnames):
     assert isinstance(expnames, (list, tuple))
     return pd.concat([df[df['expname']==el] for el in expnames], ignore_index=True)
 
+def get_by_ext(df, ext):
+    assert isinstance(ext, str)
+    return pd.concat([df[df['ext']==ext]], ignore_index=True)
 
 _pretty_names = {
     'redness': 'Color',
